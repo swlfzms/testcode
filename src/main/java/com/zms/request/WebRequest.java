04d01d4c5f25b5b9215c0be43d78a950ffacffc9
swlfzms <swlfzms@163.com>
2015-06-15 21:42:36 +0800

    just test sts

---
+++ src/main/java/com/zms/request/WebRequest.java
@@ -0,0 +1,27 @@
+package com.zms.request;
+
+import java.io.PrintWriter;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.zms.mycompany.HomeController;
+
+
+@Controller
+public class WebRequest {
+
+	private static final Logger logger = LoggerFactory.getLogger(WebRequest.class);
+	private HomeController homeController = new HomeController();
+	
+	@RequestMapping(value = "/process", method = RequestMethod.GET)
+	public void process(Locale locale, PrintWriter printWriter){
+		printWriter.println("welcome back");
+		printWriter.flush();
+	}
+}
---
+++ src/main/java/com/zms/response/User.java
@@ -0,0 +1,34 @@
+package com.zms.response;
+
+public class User {
+
+	private String username;
+	private String password;
+	private String userid;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		
+		return super.toString();
+	}
+}
